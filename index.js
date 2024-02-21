5f32bc4c1812882749e7517c9c01a0f92112fff5
Oreo1150 <sirachat2046@gmail.com>
2024-02-21 09:41:05 +0700

    d

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const app = express();
 const path = require("path");
 var bodyParser = require("body-parser");
 
-//const base_url = "http://localhost:3000";
-const base_url = "http://node57091-sirachat-noderest.proen.app.ruk-com.cloud";
+const base_url = "http://localhost:3000";
+// const base_url = "http://node57091-sirachat-noderest.proen.app.ruk-com.cloud";
 
 app.set("views", path.join(__dirname,"/public/views"));
 app.set("view engine", "ejs");
